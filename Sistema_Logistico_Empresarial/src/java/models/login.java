5efd99bc3a2c5519f50b47fd5adaea94a0fe46f6
Marco Alvarado <marcotrabajos2013@gmail.com>
2019-09-19 15:12:13 -0600

    conexion.java, usersDAO(incomplete)
    solo se crearon las clases falta el xml

--- Sistema_Logistico_Empresarial/src/java/models/login.java
+++ Sistema_Logistico_Empresarial/src/java/models/login.java
@@ -1,14 +1,47 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package models;
 
-/**
- *
- * @author Juana
- */
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean
 public class login {
+    private String username;
+    private String password;
+    
+    public login(){
+    
+    }
+    public login(String username, String password){
+        this.username = username;
+        this.password = password;
+    }
+
+    /**
+     * @return the username
+     */
+    public String getUsername() {
+        return username;
+    }
+
+    /**
+     * @param username the username to set
+     */
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    /**
+     * @return the password
+     */
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     * @param password the password to set
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
     
 }
