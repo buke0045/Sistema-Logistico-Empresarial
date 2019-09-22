b9fa1e3c1aa720caaf9148d8a1806953790aa178
Marco Alvarado <marcotrabajos2013@gmail.com>
2019-09-22 02:38:07 -0600

    Adelanto de vista Usuario
    Aun falta actualizar, eliminar, y el botón de registrar en el login

--- Sistema_Logistico_Empresarial/src/java/models/UserDAO.java
+++ Sistema_Logistico_Empresarial/src/java/models/UserDAO.java
@@ -4,6 +4,9 @@ package models;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
 public class UserDAO extends Conexion{
 
@@ -11,27 +14,54 @@ public class UserDAO extends Conexion{
         super();
     }
     
-    public void addUser(User pUser) throws Exception {
+   public void addUser(User pUser) throws Exception {
 
 	Connection myConn = null;
 	PreparedStatement myStmt = null;
-
 	try {
 		myConn = this.getConnection();
 
-		String sql = "insert into user (username, password) values (?, ?)";
-
+		String sql = "insert into user (username, password, name, surname, gender, email) values (?, ?, ?, ?, ?, ?)";
 		myStmt = myConn.prepareStatement(sql);
-
 		myStmt.setString(1, pUser.getUsername());
 		myStmt.setString(2, pUser.getPassword());
-		
+		myStmt.setString(3, pUser.getName());
+		myStmt.setString(4, pUser.getSurname());
+		myStmt.setString(5, pUser.getGender());
+		myStmt.setString(6, pUser.getEmail());
 		myStmt.execute();			
 	}
 	finally {
 		this.close(myConn, myStmt);
 	}
 		
+    }
+    public List<User> getUsers() throws Exception{
+        List<User> Users = new ArrayList<>();
+        Connection myConn = null;
+        Statement myStmt = null;
+        ResultSet myRs = null;
+        try {
+            myConn = this.getConnection();
+            String sql = "select * from user order by username";
+            myStmt = myConn.createStatement();
+            myRs = myStmt.executeQuery(sql);
+            while (myRs.next()) {
+                int id = myRs.getInt("id");
+                String username = myRs.getString("username");
+                String password = myRs.getString("password");
+                String name = myRs.getString("name");
+                String surname = myRs.getString("surname");
+                String gender = myRs.getString("gender");
+                String email = myRs.getString("email");
+                User tempUser = new User(id, username, password, name, surname, gender, email);
+                Users.add(tempUser);
+            }
+            return Users;		
+        }
+        finally {
+                this.close (myConn, myStmt, myRs);
+        }
     }
     public User getUser(String pUsername, String pPassword) throws Exception {
 	
@@ -57,8 +87,12 @@ public class UserDAO extends Conexion{
 			int id = myRs.getInt("id");
 			String username = myRs.getString("username");
 			String password = myRs.getString("password");
+                        String name = myRs.getString("name");
+                        String surname = myRs.getString("surname");
+                        String gender = myRs.getString("gender");
+                        String email = myRs.getString("email");
 
-			pUser = new User(id, username, password);
+			pUser = new User(id, username, password, name, surname, gender, email);
 		}
 		else {
 			throw new Exception("Usuario o Contraseña invalidos");
@@ -69,5 +103,30 @@ public class UserDAO extends Conexion{
 	finally {
 		this.close (myConn, myStmt, myRs);
 	}
-	}
+    }
+     public void updateUser(User pUser) throws Exception {
+
+        Connection myConn = null;
+        PreparedStatement myStmt = null;
+
+        try {
+                myConn = this.getConnection();
+
+                String sql = "update User " + " set username=?, password=?, name=?, surname=?, gender=?, email=?  where id=?";
+                myStmt = myConn.prepareStatement(sql);
+
+                myStmt.setInt(1, pUser.getId());
+                myStmt.setString(1, pUser.getUsername());
+		myStmt.setString(2, pUser.getPassword());
+		myStmt.setString(3, pUser.getName());
+		myStmt.setString(4, pUser.getSurname());
+		myStmt.setString(5, pUser.getGender());
+		myStmt.setString(6, pUser.getEmail());
+
+                myStmt.execute();
+        }
+        finally {
+                this.close (myConn, myStmt);
+        }
+    }
 }
