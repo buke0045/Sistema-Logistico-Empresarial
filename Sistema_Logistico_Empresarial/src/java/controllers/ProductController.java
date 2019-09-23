f92d28316813e13845cb12fb3277767b380b1f77
Juan <juanruizh2406@gmail.com>
2019-09-22 21:35:24 -0600

    productos funcionando pero falta añadir imagen y categoria *existente*

---
+++ Sistema_Logistico_Empresarial/src/java/controllers/ProductController.java
@@ -0,0 +1,112 @@
+
+package controllers;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
+import models.Product;
+import models.ProductDAO;
+
+// AGREGAR(falta),
+// MOSTRAR(falta),
+// MODIFICAR(falta),
+// ELIMINAR(falta)
+
+@ManagedBean
+@SessionScoped
+public class ProductController {
+    
+    private List<Product> Products;
+    private ProductDAO productDbUtil;
+    private Logger logger = Logger.getLogger(getClass().getName());
+    public ProductController()throws Exception{
+        Products = new ArrayList<>();
+        productDbUtil = ProductDAO.getInstanceP();
+    }
+    public List<Product> getProducts(){
+        return Products;
+    }
+    public void loadProducts(){
+        logger.info("Loading products");
+        Products.clear();
+        try {
+            Products = productDbUtil.getProduct();
+        } catch (Exception e) {
+            logger.log(Level.SEVERE, "Error loading users", e);
+            addErrorMessage(e);
+        }
+    }
+    public String addProduct(Product pProduct){
+        
+        try {
+            productDbUtil.addProduct(pProduct);
+        } catch (Exception e) {
+            logger.log(Level.SEVERE, "Error adding product", e);			
+            addErrorMessage(e);
+            return null;
+        }
+            return "AdmProducts?faces-redirect=true";
+    }
+    public String loadProduct(int prodCode) {
+
+            logger.info("loading product: " + prodCode);
+
+            try {
+                    Product products = productDbUtil.getProduct(prodCode);
+                    
+                    ExternalContext externalContext = FacesContext.getCurrentInstance().getExternalContext();		
+                    Map<String, Object> requestMap = externalContext.getRequestMap();
+                    requestMap.put("product", products);
+                 
+                    
+            } catch (Exception exc) {
+                    logger.log(Level.SEVERE, "Error loading product code:" + prodCode, exc);
+                    addErrorMessage(exc);
+
+                    return null;
+            }
+
+            return "update-product-form.xhtml";
+    }
+    public String updateProduct(Product pProduct) {
+            logger.info("updating User id: " + pProduct.getCode());		
+            try {			
+                    productDbUtil.updateProduct(pProduct);
+
+            } catch (Exception exc) {
+                    logger.log(Level.SEVERE, "Error updating product: " + pProduct, exc);
+                    addErrorMessage(exc);
+
+                    return null;
+            }
+
+            return "AdmProducts?faces-redirect=true";		
+    }
+    public String deleteProduct(int prodCode) {
+
+	logger.info("Deleting product code: " + prodCode);
+	
+	try {
+		productDbUtil.deleteProduct(prodCode);
+		
+	} catch (Exception exc) {
+		logger.log(Level.SEVERE, "Error deleting User id: " + prodCode, exc);
+		addErrorMessage(exc);
+		
+		return null;
+	}
+	
+	return "AdmProducts";	
+    }
+    private void addErrorMessage(Exception exc) {
+		FacesMessage message = new FacesMessage("Error: " + exc.getMessage());
+		FacesContext.getCurrentInstance().addMessage(null, message);
+    }
+}
---
+++ Sistema_Logistico_Empresarial/src/java/models/Product.java
@@ -0,0 +1,128 @@
+
+package models;
+
+import java.io.File;
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean
+public class Product {
+    private int code;
+    private String description;
+    private String category;
+    private int currentExist;
+    private int minExist;
+    private int maxExist;
+    private File image;
+    
+    public Product(){
+    
+    }
+    public Product(int code, String description, String category, int currentExist, int minExist, int maxExist, File image){
+        this.code = code;
+        this.description = description;
+        this.category = category;
+        this.currentExist = currentExist;
+        this.minExist = minExist;
+        this.maxExist = maxExist;
+        this.image = image;
+    }
+
+    /**
+     * @return the code
+     */
+    public int getCode() {
+        return code;
+    }
+
+    /**
+     * @param code the code to set
+     */
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    /**
+     * @return the description
+     */
+    public String getDescription() {
+        return description;
+    }
+
+    /**
+     * @param description the description to set
+     */
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    /**
+     * @return the category
+     */
+    public String getCategory() {
+        return category;
+    }
+
+    /**
+     * @param category the category to set
+     */
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    /**
+     * @return the currentExist
+     */
+    public int getCurrentExist() {
+        return currentExist;
+    }
+
+    /**
+     * @param currentExist the currentExist to set
+     */
+    public void setCurrentExist(int currentExist) {
+        this.currentExist = currentExist;
+    }
+
+    /**
+     * @return the minExist
+     */
+    public int getMinExist() {
+        return minExist;
+    }
+
+    /**
+     * @param minExist the minExist to set
+     */
+    public void setMinExist(int minExist) {
+        this.minExist = minExist;
+    }
+
+    /**
+     * @return the maxExist
+     */
+    public int getMaxExist() {
+        return maxExist;
+    }
+
+    /**
+     * @param maxExist the maxExist to set
+     */
+    public void setMaxExist(int maxExist) {
+        this.maxExist = maxExist;
+    }
+
+    /**
+     * @return the image
+     */
+    public File getImage() {
+        return image;
+    }
+
+    /**
+     * @param image the image to set
+     */
+    public void setImage(File image) {
+        this.image = image;
+    }
+    
+}
